f391c83d4e2a0383854a15f057b10c3eb35c036c
Flooded0 <63896468+Flooded0@users.noreply.github.com>
2023-08-17 09:48:20 +1000

    Update TFormat.lua

--- TFormat.lua
+++ TFormat.lua
@@ -24,64 +24,18 @@ local StringRet = function(Object, Typ)
     return Ret;
 end;
 local function FormatValue(Value)
-    local Typ = type(Value);
-
-    local Handlers = {
-        string = function() return '"' .. Value .. '"' end,
-        Instance = function() return Value:GetFullName() end,
-        Vector3 = function() return ("Vector3.new(%s, %s, %s)"):format(Value.X, Value.Y, Value.Z) end,
-        Vector2 = function() return ("Vector2.new(%s, %s)"):format(Value.X, Value.Y) end,
-        CFrame = function() return ("CFrame.new(%s, %s, %s, %s, %s, %s, %s, %s, %s)"):format(Value:components()) end,
-        Color3 = function() return ("Color3.fromRGB(%d, %d, %d)"):format(Value.R * 255, Value.G * 255, Value.B * 255) end,
-        UDim2 = function() return ("UDim2.new(%d, %d, %d, %d)"):format(Value.X.Scale, Value.X.Offset, Value.Y.Scale, Value.Y.Offset) end,
-        UDim = function() return ("UDim.new(%d, %d)"):format(Value.Scale, Value.Offset) end,
-        NumberRange = function() return ("NumberRange.new(%s, %s)"):format(Value.Min, Value.Max) end,
-        Rect = function() return ("Rect.new(%s, %s, %s, %s)"):format(Value.Min.X, Value.Min.Y, Value.Max.X, Value.Max.Y) end,
-        boolean = function() return Value and "true" or "false" end,
-        NumberSequence = function() return ("NumberSequence.new(%s)"):format(table.concat(Value.Keyframes, ", ")) end,
-        ColorSequence = function()
-            local ColorTable = {}; for _, Keyframe in ipairs(Value.Keypoints) do table.insert(ColorTable, ("ColorSequenceKeypoint.new(%s, Color3.fromRGB(%d, %d, %d))"):format(Keyframe.Time, Keyframe.Value.R * 255, Keyframe.Value.G * 255, Keyframe.Value.B * 255)); end;
-            return ("ColorSequence.new{%s}"):format(table.concat(ColorTable, ", "));
-        end,
-        BrickColor = function() return ("BrickColor.new(%d)"):format(Value.Number) end,
-        Ray = function() return ("Ray.new(%s, %s)"):format(FormatValue(Value.Origin), FormatValue(Value.Direction)) end,
-    };
-
-    Handlers.Enum = function() return ("%s.%s"):format(Value.EnumType.Name, Value.Name) end;
-    Handlers.EnumItem = function() return ("%s.%s"):format(Value.EnumType.Name, Value.Name) end;
-    Handlers.TweenInfo = function()
-        if typeof(Value.EasingStyle) == "EnumItem" and typeof(Value.EasingDirection) == "EnumItem" then
-            local Fields = {
-                ("Time = %f"):format(Value.Time),
-                ("EasingStyle = Enum.EasingStyle.%s"):format(Value.EasingStyle.Name),
-                ("EasingDirection = Enum.EasingDirection.%s"):format(Value.EasingDirection.Name)
-            };
-            return ("TweenInfo.new(%s)"):format(table.concat(Fields, ", "));
-        else
-            return "TweenInfo.new()";
-        end;
-    end;
-    Handlers.Random = function() return ("Random.new(%d)"):format(Value.Seed) end;
-    Handlers.Axes = function()
-        local AxesTable = {}; for _, Axis in ipairs(Enum.Axis:GetEnumItems()) do if Value[Axis.Name] then table.insert(AxesTable, ("Enum.Axis.%s"):format(Axis.Name)); end; end;
-        return ("Axes.new(%s)"):format(table.concat(AxesTable, ", "));
-    end;
-    Handlers.Faces = function()
-        local FacesTable = {}; for _, Face in ipairs(Enum.NormalId:GetEnumItems()) do if Value[Face.Name] then table.insert(FacesTable, ("Enum.NormalId.%s"):format(Face.Name)); end; end;
-        return ("Faces.new(%s)"):format(table.concat(FacesTable, ", "));
-    end;
+    local Typ = Type(Value);
 
     if StrTypes[Typ] then
         return StringRet(Value, Typ);
-    elseif Handlers[Typ] then
-        return Handlers[Typ]();
-    elseif typeof(Value) == "Enum" then
-        return Handlers.Enum();
+    elseif Typ == "string" then
+        return '"'.. Value ..'"';
+    elseif Typ == "Instance" then
+        return Value.GetFullName(Value);
     else
-        --return ("%q"):format(tostring(Value));
-        return ("%s.new(%s)"):format(Typ, tostring(Value));
+        return Typ .. ".new(" .. tostring(Value) .. ")";
     end;
-end
+end;
 
 SerializeTable = function(Table, Padding, Cache, StringRep)
     local Count, Str, Num = 1, {}, #Table or CountTable(Table);
